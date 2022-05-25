630101710dfa45b24a4508b8d12d610fd72d3896
Rob Emanuele <rdemanuele@gmail.com>
2022-05-25 10:06:22 -0400

    Ensure no trailing dash in batch id

--- pctasks/execute/pctasks/execute/batch/utils.py
+++ pctasks/execute/pctasks/execute/batch/utils.py
@@ -8,7 +8,7 @@ def make_unique_job_id(job_id: str) -> str:
     )
 
 
-def make_valid_batch_id(job_id: str) -> str:
+def make_valid_batch_id(id: str) -> str:
     """Returns a job id or task id that is valid to Batch
 
     Note from Azure Batch SDK:
@@ -18,7 +18,7 @@ def make_valid_batch_id(job_id: str) -> str:
         and underscore (_).
         The name must be from 1 through 64 characters long
     """
-    id = re.sub("[^a-zA-Z0-9_-]", "-", job_id)
+    id = re.sub("[^a-zA-Z0-9_-]", "-", id)
     if len(id) > 64:
         id = id[:32] + "-" + id[-31:]
-    return id
+    return id.strip("-")
