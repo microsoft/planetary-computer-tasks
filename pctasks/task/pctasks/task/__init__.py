02e575bc093729aa23aa17a2e9ee47cf25967658
Rob Emanuele <rdemanuele@gmail.com>
2022-05-31 05:10:25 -0400

    __version__ import needs to be at top

--- pctasks/task/pctasks/task/__init__.py
+++ pctasks/task/pctasks/task/__init__.py
@@ -1,5 +1,9 @@
+""" pctasks.task
+
+isort:skip_file
+"""
+from pctasks.task.version import __version__
 from pctasks.task.context import TaskContext
 from pctasks.task.task import Task
-from pctasks.task.version import __version__
 
 __all__ = ["__version__", "Task", "TaskContext"]
