52c4c2f9042474789d5974486d4d238ea0642c9b
Matt McFarland <mmcfarland@microsoft.com>
2022-08-26 11:08:05 -0400

    Update deployment and documentation
    Terraform changes to apply access token validation against the
    documented app registrations.

--- deployment/terraform/dev/main.tf
+++ deployment/terraform/dev/main.tf
@@ -25,6 +25,11 @@ module "resources" {
   pctasks_server_sp_client_secret = var.pctasks_server_sp_client_secret
   pctasks_server_sp_object_id = var.pctasks_server_sp_object_id
 
+  deploy_secrets_kv_name = var.deploy_secrets_kv_name
+  deploy_secrets_kv_rg = var.deploy_secrets_kv_rg
+  access_key_secret_name = var.access_key_secret_name
+  backend_api_app_id_secret_name = var.backend_api_app_id_secret_name
+
   k8s_version = "1.23.5"
   k8s_orchestrator_version = "1.23.5"
 
