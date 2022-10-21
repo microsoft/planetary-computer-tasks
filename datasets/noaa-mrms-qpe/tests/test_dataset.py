00d03afbefb3ec2f6b6b8310f81fd1a292f86456
pjhartzell <preston.hartzell@gmail.com>
2022-10-21 14:12:26 -0400

    use pytest and parametrize

--- datasets/noaa-mrms-qpe/tests/test_dataset.py
+++ datasets/noaa-mrms-qpe/tests/test_dataset.py
@@ -1,17 +1,21 @@
-import logging
 from pathlib import Path
 
-from pctasks.cli.cli import setup_logging, setup_logging_for_module
+import pytest
+
 from pctasks.dev.test_utils import run_process_items_workflow
 
 HERE = Path(__file__).parent
 DATASET_PATH = HERE / "dataset-dev-cluster.yaml"
 
 
-def test_1h_pass1():
+@pytest.mark.parametrize(
+    "collection",
+    ["noaa-mrms-qpe-1h-pass1", "noaa-mrms-qpe-1h-pass2", "noaa-mrms-qpe-24h-pass2"],
+)
+def test_collection(collection):
     run_process_items_workflow(
         DATASET_PATH,
-        "noaa-mrms-qpe-1h-pass1",
+        collection,
         args={
             "registry": "localhost:5001",
         },
@@ -19,39 +23,3 @@ def test_1h_pass1():
         chunks_limit=2,
         timeout_seconds=600,
     )
-
-
-def test_1h_pass2():
-    run_process_items_workflow(
-        DATASET_PATH,
-        "noaa-mrms-qpe-1h-pass2",
-        args={
-            "registry": "localhost:5001",
-        },
-        splits_limit=1,
-        chunks_limit=2,
-        timeout_seconds=600,
-    )
-
-
-def test_24h_2pass():
-    run_process_items_workflow(
-        DATASET_PATH,
-        "noaa-mrms-qpe-24h-pass2",
-        args={
-            "registry": "localhost:5001",
-        },
-        splits_limit=1,
-        chunks_limit=2,
-        timeout_seconds=600,
-    )
-
-
-if __name__ == "__main__":
-    setup_logging(logging.DEBUG)
-    setup_logging_for_module("__main__", logging.DEBUG)
-    test_1h_pass1()
-    test_1h_pass2()
-    test_24h_2pass()
-    print("All tests passed")
-    exit(0)
