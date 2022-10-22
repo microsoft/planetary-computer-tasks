cc98e31f4fc98c29337682231933bde36f9a8324
pjhartzell <preston.hartzell@gmail.com>
2022-10-22 06:37:41 -0400

    update to production cog container

--- datasets/noaa-mrms-qpe/noaa_mrms_qpe.py
+++ datasets/noaa-mrms-qpe/noaa_mrms_qpe.py
@@ -10,8 +10,7 @@ from pctasks.core.models.task import WaitTaskResult
 from pctasks.core.storage import StorageFactory
 from pctasks.dataset.collection import Collection
 
-# COG_CONTAINER = "blob://mrms/mrms-cogs"
-COG_CONTAINER = "blob://devstoreaccount1/mrms-cogs"
+COG_CONTAINER = "blob://mrms/mrms-cogs"
 
 
 class NoaaMrmsQpeCollection(Collection):
