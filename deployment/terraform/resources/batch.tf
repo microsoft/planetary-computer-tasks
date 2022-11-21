08fee637fa82902c779d05327fb8241c8a98601d
Tom Augspurger <taugspurger@microsoft.com>
2022-11-21 16:08:43 -0600

    [deploy] Make batch accounts private
    Sets the account-level flag for storage accounts to private,
    in addition to the container-level settings.

--- deployment/terraform/resources/batch.tf
+++ deployment/terraform/resources/batch.tf
@@ -1,9 +1,10 @@
 resource "azurerm_storage_account" "pctasks-batch" {
-  name                     = substr("stb${local.nodash_prefix}", 0, 24)
-  resource_group_name      = azurerm_resource_group.pctasks.name
-  location                 = azurerm_resource_group.pctasks.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = substr("stb${local.nodash_prefix}", 0, 24)
+  resource_group_name             = azurerm_resource_group.pctasks.name
+  location                        = azurerm_resource_group.pctasks.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
 }
 
 resource "azurerm_batch_account" "pctasks" {
