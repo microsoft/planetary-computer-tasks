ab03f1029963cb2b006e3caac8b10d033087c3ad
pjhartzell <preston.hartzell@gmail.com>
2022-12-06 11:38:24 -0500

    move gridded collection create item feeder logic to stactools package

--- datasets/noaa-climate-normals/noaa_climate_normals.py
+++ datasets/noaa-climate-normals/noaa_climate_normals.py
@@ -10,7 +10,7 @@ from stactools.noaa_climate_normals.gridded.constants import (
 )
 from stactools.noaa_climate_normals.gridded.constants import Period as GriddedPeriod
 from stactools.noaa_climate_normals.gridded.stac import (
-    create_item as gridded_create_item,
+    create_items as gridded_create_items,
 )
 from stactools.noaa_climate_normals.gridded.utils import nc_href_dict
 from stactools.noaa_climate_normals.netcdf.stac import create_item as netcdf_create_item
@@ -25,6 +25,7 @@ from stactools.noaa_climate_normals.tabular.stac import (
 from pctasks.core.models.task import WaitTaskResult
 from pctasks.core.storage import StorageFactory
 from pctasks.dataset.collection import Collection
+from pctasks.core.storage.base import Storage
 
 PARQUET_CONTAINER = "blob://noaanormals/climate-normals-geoparquet"
 COG_CONTAINER = "blob://noaanormals/gridded-normals-cogs"
@@ -63,6 +64,7 @@ class NoaaClimateNormalsTabular(Collection):
         )
 
         with TemporaryDirectory() as tmp_dir:
+            # download all CSVs, retrying on connection resets and hanging downloads
             tmp_csv_paths = []
             failures = True
             retry_num = 0
@@ -99,6 +101,7 @@ class NoaaClimateNormalsTabular(Collection):
                 else:
                     failures = False
 
+            # create Item
             logger.info("Creating Item and GeoParquet")
             item = tabular_create_item(
                 csv_hrefs=tmp_csv_paths,
@@ -108,6 +111,7 @@ class NoaaClimateNormalsTabular(Collection):
                 num_partitions=5,
             )
 
+            # upload created GeoParquet to Azure and update Item asset href
             logger.info("Uploading GeoParquet")
             parquet_path = Path(tmp_dir, parquet_directory)
             if not parquet_path.exists():
@@ -158,72 +162,41 @@ class NoaaClimateNormalsGridded(Collection):
             Path.mkdir(tmp_nc_dir)
             Path.mkdir(tmp_cog_dir)
 
-            # download the netcdfs
+            # download the netcdfs to avoid repeated remote data calls
             all_nc_uris = nc_href_dict(asset_uri, frequency)
             for nc_uri in all_nc_uris.values():
                 nc_storage, nc_path = storage_factory.get_storage_for_file(nc_uri)
                 tmp_nc_path = Path(tmp_nc_dir, Path(nc_path).name)
                 nc_storage.download_file(nc_path, tmp_nc_path)
 
-            # prep for item and cog creation
+            # create all items
+            all_items = gridded_create_items(
+                nc_href=tmp_nc_path,
+                cog_dir=tmp_cog_dir,
+                api_url_netcdf=NETCDF_STAC_API_URL
+            )
+
+            # upload created COGs to Azure and update Item asset hrefs
+            def get_cog_storage(_frequency: str) -> Storage:
+                return storage_factory.get_storage(
+                    f"{COG_CONTAINER}/normals-{_frequency}/{period}/"
+                )
+            cog_storage_dict = {}
             if frequency is GriddedFrequency.DAILY:
-                num_cogs = 6 * 366
-                data = {
-                    "daily": {
-                        "frequency": GriddedFrequency.DAILY,
-                        "indices": range(1, 367),
-                    }
-                }
-            elif frequency is GriddedFrequency.MLY:
-                num_cogs = 4 * 20 + 12 * 20 + 1 * 20
-                data = {
-                    "monthly": {
-                        "frequency": GriddedFrequency.MLY,
-                        "indices": range(1, 13),
-                    },
-                    "seasonal": {
-                        "frequency": GriddedFrequency.SEAS,
-                        "indices": range(1, 5),
-                    },
-                    "annual": {"frequency": GriddedFrequency.ANN, "indices": [1]},
-                }
+                cog_storage_dict["daily"] = get_cog_storage("daily")
             else:
-                raise ValueError(f"Unexpected frequency: {frequency.name}")
+                cog_storage_dict["monthly"] = get_cog_storage("monthly")
+                cog_storage_dict["seasonal"] = get_cog_storage("seasonal")
+                cog_storage_dict["annual"] = get_cog_storage("annual")
 
-            # create items and cogs
-            all_items: List[pystac.Item] = []
-            for key, value in data.items():
-                items = []
-                for index in value["indices"]:
-                    items.append(
-                        gridded_create_item(
-                            nc_href=tmp_nc_path,
-                            frequency=value["frequency"],
-                            time_index=index,
-                            cog_dir=tmp_cog_dir,
-                            api_url_netcdf=NETCDF_STAC_API_URL,
-                        )
+            for item in all_items:
+                for asset_key, asset_value in item.assets.items():
+                    cog_filename = f"{item.id}-{asset_key}.tif"
+                    _frequency = item.id.split("-")[1]
+                    cog_storage = cog_storage_dict[_frequency]
+                    cog_storage.upload_file(
+                        Path(tmp_cog_dir, cog_filename), cog_filename
                     )
-
-                cog_storage = storage_factory.get_storage(
-                    f"{COG_CONTAINER}/normals-{key}/{period}/"
-                )
-
-                for item in items:
-                    for asset_key, asset_value in item.assets.items():
-                        cog_filename = f"{item.id}-{asset_key}.tif"
-                        cog_storage.upload_file(
-                            Path(tmp_cog_dir, cog_filename), cog_filename
-                        )
-                        asset_value.href = cog_storage.get_url(cog_filename)
-
-                all_items.extend(items)
-
-            local_cogs = list(tmp_cog_dir.glob("*.tif"))
-            if len(local_cogs) != num_cogs:
-                raise ValueError(
-                    f"Incorrect number of COGs produced: "
-                    f"{num_cogs} expected, but {len(local_cogs)} produced."
-                )
+                    asset_value.href = cog_storage.get_url(cog_filename)
 
         return all_items
