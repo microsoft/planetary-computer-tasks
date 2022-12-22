02e9a81465291dc6d90d2744bafd107fa5b97a94
pjhartzell <preston.hartzell@gmail.com>
2022-12-22 15:40:12 -0500

    feat: add esa cci land cover dataset

---
+++ datasets/esa-cci-lc/collection/esa-cci-lc/description.md
@@ -0,0 +1,4 @@
+
+The ESA Climate Change Initiative (CCI) [Land Cover dataset](https://cds.climate.copernicus.eu/cdsapp#!/dataset/satellite-land-cover?tab=overview) provides consistent global annual land cover maps at 300m spatial resolution from 1992 to 2020. The land cover classes are defined using the United Nations Food and Agriculture Organization's (UN FAO) [Land Cover Classification System](https://www.fao.org/land-water/land/land-governance/land-resources-planning-toolbox/category/details/en/c/1036361/) (LCCS). In addition to the land cover maps, four quality flags are produced to document the reliability of the classification and change detection. 
+
+The data in this Collection have been converted from the [original NetCDF data](https://planetarycomputer.microsoft.com/dataset/esa-cci-lc-netcdf) to a set of tiled [Cloud Optimized GeoTIFFs](https://www.cogeo.org/) (COGs).
---
+++ datasets/esa-cci-lc/esa_cci_lc.py
@@ -0,0 +1,68 @@
+from tempfile import TemporaryDirectory
+from typing import List, Union
+from pathlib import Path
+
+import pystac
+from stactools.esa_cci_lc.cog.stac import create_items as cog_create_items
+from stactools.esa_cci_lc.netcdf.stac import create_item as netcdf_create_item
+
+from pctasks.core.models.task import WaitTaskResult
+from pctasks.core.storage import StorageFactory
+from pctasks.dataset.collection import Collection
+
+
+class EsaCciLcCog(Collection):
+    @classmethod
+    def create_item(
+        cls, asset_uri: str, storage_factory: StorageFactory
+    ) -> Union[List[pystac.Item], WaitTaskResult]:
+
+        with TemporaryDirectory() as tmp_dir:
+            storage, nc_path = storage_factory.get_storage_for_file(asset_uri)
+            tmp_nc_path = str(Path(tmp_dir, Path(nc_path).name))
+            storage.download_file(nc_path, tmp_nc_path)
+
+            items = cog_create_items(
+                nc_path=tmp_nc_path,
+                cog_dir=tmp_dir,
+                nc_api_url=(
+                    "https://planetarycomputer.microsoft.com/api/stac/v1/"
+                    "collections/esa-cci-lc-netcdf/items"
+                ),
+            )
+
+            for item in items:
+                id_parts = item.id.split("-")
+                tile = id_parts[-1]
+                version = id_parts[-2]
+                year = id_parts[-3]
+                for asset in item.assets.values():
+                    asset_path = str(
+                        Path(nc_path).parent.parent
+                        / "cog"
+                        / version
+                        / tile
+                        / year
+                        / Path(asset.href).name
+                    )
+                    storage.upload_file(asset.href, asset_path)
+                    asset.href = storage.get_url(asset_path)
+
+        return items
+
+
+class EsaCciLcNetcdf(Collection):
+    @classmethod
+    def create_item(
+        cls, asset_uri: str, storage_factory: StorageFactory
+    ) -> Union[List[pystac.Item], WaitTaskResult]:
+
+        with TemporaryDirectory() as tmp_dir:
+            storage, nc_path = storage_factory.get_storage_for_file(asset_uri)
+            tmp_nc_path = str(Path(tmp_dir, Path(nc_path).name))
+            storage.download_file(nc_path, tmp_nc_path)
+
+            item = netcdf_create_item(tmp_nc_path)
+            item.assets["netcdf"].href = storage.get_url(nc_path)
+
+        return [item]
