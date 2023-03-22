f70830e07aaf5572a8cfdde518a61b60e77573d9
pjhartzell <preston.hartzell@gmail.com>
2023-03-22 10:39:09 -0400

    debugging: add some logging, fix some paths

--- datasets/esa-worldcover/esa_worldcover.py
+++ datasets/esa-worldcover/esa_worldcover.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import List, Union
@@ -9,6 +10,13 @@ from pctasks.core.models.task import WaitTaskResult
 from pctasks.core.storage import StorageFactory
 from pctasks.dataset.collection import Collection
 
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("[%(levelname)s]:%(asctime)s: %(message)s"))
+handler.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 
 class ESAWorldCoverCollection(Collection):
     @classmethod
@@ -16,20 +24,31 @@ class ESAWorldCoverCollection(Collection):
         cls, asset_uri: str, storage_factory: StorageFactory
     ) -> Union[List[pystac.Item], WaitTaskResult]:
         with TemporaryDirectory() as tmp_dir:
+            logger.info(f"asset uri = {asset_uri}")
             storage, map_path = storage_factory.get_storage_for_file(asset_uri)
-            tmp_map_path = str(Path(tmp_dir, Path(map_path).name))
-            inputquality_path = map_path.replace("_Map.tif", "_InputQuality.tif")
-            tmp_inputquality_path = tmp_map_path.replace(
-                "_Map.tif", "_InputQuality.tif"
-            )
+            logger.info(f"map path = {map_path}")
+            map_name = Path(map_path).name
+            tmp_map_path = str(Path(tmp_dir, map_name))
+
+            quality_name = map_name.replace("_Map.tif", "_InputQuality.tif")
+            quality_path = str(Path(map_path).parent.parent / "input_quality" / quality_name)
+            logger.info(f"quality path = {quality_path}")
+            tmp_quality_path = str(Path(tmp_dir, quality_name))
 
             storage.download_file(map_path, tmp_map_path)
-            storage.download_file(inputquality_path, tmp_inputquality_path)
+            storage.download_file(quality_path, tmp_quality_path)
 
-            item = create_item(map_href=tmp_map_path, include_quality_asset=True)
+            item = create_item(
+                map_href=tmp_map_path, include_quality_asset=True, raster_footprint=True
+            )
 
             for key, value in item.assets.items():
-                asset_path = str(Path(map_path).parent / Path(value.href).name)
+                if key == "map":
+                    asset_path = map_path
+                elif key == "input_quality":
+                    asset_path = quality_path
+                else:
+                    raise ValueError(f"Unexpected Item asset key '{key}'")
                 item.assets[key].href = storage.get_url(asset_path)
 
         return [item]
