09909147d06952e78dbf12f0a3e9bae4c163c503
pjhartzell <preston.hartzell@gmail.com>
2023-04-17 10:49:35 -0400

    update sentinel-3 synergy aod collection

---
+++ datasets/sentinel-3/collection/sentinel-3-synergy-aod-l2-netcdf/description.md
@@ -0,0 +1,11 @@
+This Collection contains the Sentinel-3 Level-2 [Synergy Aerosol Optical Depth](https://sentinels.copernicus.eu/web/sentinel/level-2-aod) (SY_2_AOD) product , which is a downstream development of the Sentinel-2 Level-1 [OLCI Full Resolution](https://sentinels.copernicus.eu/web/sentinel/user-guides/sentinel-3-olci/data-formats/level-1) (OL_1_EFR) and [SLSTR Radiances and Brightness Temperatures](https://sentinels.copernicus.eu/web/sentinel/user-guides/Sentinel-3-slstr/data-formats/level-1) (SL_1_RBT__) products. The dataset contains both retrieved and diagnostic global aerosol parameters at super-pixel (4.5 km x 4.5 km) resolution for all regions over land and ocean free of snow/ice cover, excluding high cloud fraction data. The retrieved and derived aerosol parameters are:
+
+- Aerosol Optical Depth (AOD) at 440, 550, 670, 985, 1600 and 2250 nm
+- Error estimates (i.e. standard deviation) in AOD at 440, 550, 670, 985, 1600 and 2250 nm
+- Single Scattering Albedo (SSA) at 440, 550, 670, 985, 1600 and 2250 nm
+- Fine-mode AOD at 550nm
+- Aerosol Angstrom parameter between 550 and 865nm
+- Dust AOD at 550nm
+- Aerosol absorption optical depth at 550nm
+
+Atmospherically corrected nadir surface directional reflectances at 440, 550, 670, 985, 1600 and 2250 nm at super-pixel (4.5 km x 4.5 km) resolution are also provided. The dataset is available in NetCDF format from April 2020 to present.
--- datasets/sentinel-3/sentinel_3/sentinel_3_synergy_aod_l2_netcdf.py
+++ datasets/sentinel-3/sentinel_3/sentinel_3_synergy_aod_l2_netcdf.py
@@ -1,13 +1,16 @@
+from pathlib import Path
 from typing import List, Union
 
+import antimeridian
 import pystac
+import shapely.geometry
 
 import pctasks.dataset.collection
 from pctasks.core.models.task import WaitTaskResult
 from pctasks.core.storage import StorageFactory
 
 
-class SynergyAODCollection(pctasks.dataset.collection.Collection):
+class Collection(pctasks.dataset.collection.Collection):
     @classmethod
     def create_item(
         cls, asset_uri: str, storage_factory: StorageFactory
@@ -17,7 +20,15 @@ class SynergyAODCollection(pctasks.dataset.collection.Collection):
         storage, json_path = storage_factory.get_storage_for_file(asset_uri)
         item_dict = storage.read_json(json_path)
 
-        # FIXME: Item id contains trailing underscores to pad out a 17 character
+        # Skip item if it is not a NT (Not Time critical) product
+        asset_directory = Path(item_dict["assets"]["safe-manifest"]["href"]).parent.name
+        assert asset_directory.endswith(".SEN3")
+        timeliness = asset_directory[-11:-9]
+        assert timeliness in ["NR", "ST", "NT"]
+        if asset_directory[-11:-9] != "NT":
+            return []
+
+        # Item id contains trailing underscores to pad out a 17 character
         # instance id
         item_dict["id"] = item_dict["id"].rstrip("_")
 
@@ -38,27 +49,41 @@ class SynergyAODCollection(pctasks.dataset.collection.Collection):
             pystac.utils.str_to_datetime(properties["datetime"])
         )
 
-        # FIXME: "SYNERGY" is not a instrument
-        properties.pop("instrument", None)
+        # "SYNERGY" is not a instrument
+        properties.pop("instruments", None)
 
-        # FIXME: Do we really want to include s3:mode, which is set to EO
-        # (Earth Observation)? It offers no additional information, IMO.
+        # Do we really want to include s3:mode, which is set to EO (Earth
+        # Observation)? It offers no additional information, IMO.
         properties.pop("s3:mode", None)
 
-        # FIXME: Do we really want to include s3:gsd for the OLCI and SLSTR
-        # instruments? This is a custom object that is unlikely to be searched.
+        # Do we really want to include s3:gsd for the OLCI and SLSTR
+        # instruments? AOD is a derived product delivered at different resolution
+        # than either the OLCI or SLSTR sensor GSDs. Also, s3:gsd is a *custom*
+        # field with two gsd values that, IMO, is unlikely to be searched.
         properties.pop("s3:gsd", None)
 
-        # FIXME: Use underscores instead of camel case for consistency
-        properties["sd:product_type"] = properties.pop("sd:productType")
-        properties["s3:saline_water_pixels_percentage"] = properties.pop(
-            "s3:salineWaterPixels_percentage"
-        )
-        properties["s3:land_pixels_percentage"] = properties.pop(
-            "s3:landPixels_percentage"
-        )
+        # I'm not sure what to do with the s3:shape information. It would
+        # normally be included in the proj extension, but we are not including
+        # that extension. I see the synergy-syn product uses a syn:shape field
+        # in a different format on the asset. So there is no consistency. I'm
+        # moving it to the asset as a custom field for now, with the intent to
+        # match this layout in all the synergy products.
+        shape = properties.pop("s3:shape")
 
-        item_dict["properties"] = properties
+        # Use underscores instead of camel case for consistency
+        new_properties = {}
+        for key in properties:
+            if key.startswith("s3:"):
+                new_key = "".join(
+                    "_" + char.lower() if char.isupper() else char for char in key
+                )
+                if new_key.endswith("_pixels_percentage"):
+                    new_key = new_key.replace("_pixels_percentage", "")
+                new_properties[new_key] = properties[key]
+            else:
+                new_properties[key] = properties[key]
+
+        item_dict["properties"] = new_properties
 
         # ---- LINKS ----
         links = item_dict.pop("links")
@@ -73,32 +98,41 @@ class SynergyAODCollection(pctasks.dataset.collection.Collection):
         # ---- ASSETS ----
         assets = item_dict.pop("assets")
 
-        # FIXME: Make asset slugs consistent (underscores and lowercase)
-        assets["safe_manifest"] = assets.pop("safe-manifest")
-        assets["ntc_aod"] = assets.pop("NTC_AOD")
+        # Make asset slugs consistent (underscores and lowercase)
+        new_assets = {}
+        for key in assets:
+            new_key = key.lower().replace("-", "_")
+            new_assets[new_key] = assets[key]
+        assets = new_assets
 
-        # Remove local_path and clean up titles
-        assets["safe_manifest"]["title"] = "SAFE product manifest"
+        # Remove local_path and clean up descriptions
+        assets["safe_manifest"]["description"] = "SAFE product manifest"
         assets["safe_manifest"].pop("file:local_path", None)
-        assets["ntc_aod"]["title"] = "Surface Reflectance and Global Aerosol Parameter"
-        assets["ntc_aod"].pop("description", None)
+
+        assets["ntc_aod"][
+            "description"
+        ] = "Global aerosol parameters and surface reflectance"
         assets["ntc_aod"].pop("file:local_path", None)
 
-        # FIXME: Since this is an Aerosol product (that includes reflectance
-        # data), it seems odd to include include eo:bands. But I could be
-        # persuaded otherwise.
-        assets["ntc_aod"].pop("eo:bands", None)
+        # Add the custom shape field we peeled off the properties
+        assets["ntc_aod"]["s3:shape"] = shape
 
-        # FIXME: There is a lonely "resolution" field on the ntc_aod asset that
-        # is not part of the STAC spec. This would normally go in "raster:bands",
-        # but we are not using that extension. Propose moving this to Item
-        # properties under "s3:spatial_resolution", which matches Sentinel-5P.
-        spatial_resolution = assets["ntc_aod"].pop("resolution")
-        assert len(spatial_resolution) == 2
-        item_dict["properties"]["s3:spatial_resolution"] = f"{spatial_resolution[0]}m"
+        # Make it clear that resolution is a custom field
+        assets["ntc_aod"]["s3:resolution"] = assets["ntc_aod"].pop("resolution")
+
+        # The eo:bands data should be in micrometers (it is in nanometers)
+        for band in assets["ntc_aod"]["eo:bands"]:
+            band["center_wavelength"] = band["center_wavelength"] / 1000
+            band["full_width_half_max"] = band["band_width"] / 1000
+            band.pop("band_width")
 
         item_dict["assets"] = assets
 
-        # TODO: Fix geometry
+        # ---- GEOMETRY ----
+        item = pystac.Item.from_dict(item_dict)
+        polygon = shapely.geometry.shape(item.geometry)
+        geometry = antimeridian.fix_polygon(polygon)
+        item.bbox = list(geometry.bounds)
+        item.geometry = shapely.geometry.mapping(geometry)
 
-        return [pystac.Item.from_dict(item_dict)]
+        return [item]
