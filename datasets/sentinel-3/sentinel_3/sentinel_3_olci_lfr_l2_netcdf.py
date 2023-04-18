b9d9b15a13b94e37beaea4e1d5954b40e923e8ec
Tom Augspurger <taugspurger@microsoft.com>
2023-04-18 15:05:58 -0500

    [olci-lfr]: fixups

--- datasets/sentinel-3/sentinel_3/sentinel_3_olci_lfr_l2_netcdf.py
+++ datasets/sentinel-3/sentinel_3/sentinel_3_olci_lfr_l2_netcdf.py
@@ -1,4 +1,5 @@
 import decimal
+from pathlib import Path
 from typing import List, Union
 
 import pystac
@@ -15,13 +16,20 @@ class Collection(pctasks.dataset.collection.Collection):
     ) -> Union[List[pystac.Item], WaitTaskResult]:
         storage, json_path = storage_factory.get_storage_for_file(asset_uri)
         item_dict = storage.read_json(json_path)
+        asset_directory = Path(item_dict["assets"]["safe-manifest"]["href"]).parent.name
+        assert asset_directory.endswith(".SEN3")
 
-        # FIXME: Item id contains trailing underscores to pad out a 17 character
+        # Item id contains trailing underscores to pad out a 17 character
         # instance id
         item_dict["id"] = item_dict["id"].rstrip("_")
 
         # ---- PROPERTIES ----
         properties = item_dict.pop("properties")
+
+        # Add the processing timelessness to the properties
+        timeliness = asset_directory[-11:-9]
+        assert timeliness in ["NR", "ST", "NT"]
+        properties["s3:processing_timeliness"] = timeliness
 
         # Providers should be supplied in the collection, not the item
         properties.pop("providers", None)
@@ -37,16 +45,19 @@ class Collection(pctasks.dataset.collection.Collection):
             pystac.utils.str_to_datetime(properties["datetime"])
         )
 
-        # FIXME: Do we really want to include s3:mode, which is set to EO
-        # (Earth Observation)? It offers no additional information, IMO.
+        # Do we really want to include s3:mode, which is set to EO (Earth
+        # Observation)? It offers no additional information, IMO.
         properties.pop("s3:mode", None)
 
         # FIXME: Do we really want to include s3:gsd for the OLCI and SLSTR
         # instruments? This is a custom object that is unlikely to be searched.
         properties.pop("s3:gsd", None)
 
-        # FIXME: Use underscores instead of camel case for consistency
+        # Add a friendly product name for filtering
+        # e.g. 'OL_2_LFR___' -> 'LFR'
+        properties["s3:product_name"] = properties["s3:productType"][5:8]
 
+        # Use underscores instead of camel case for consistency
         properties["s3:product_type"] = properties.pop("s3:productType")
         properties["s3:saline_water"] = properties.pop(
             "s3:salineWaterPixels_percentage"
@@ -65,8 +76,8 @@ class Collection(pctasks.dataset.collection.Collection):
         properties["s3:dubious"] = properties.pop("s3:dubiousSamples_percentage")
 
         # use raster for invalid_pixels
-        valid_percent = 1 - properties.pop("s3:invalidPixels_percentage")
-        item_dict["raster:bands"] = [{"statistics": {"valid_percent": valid_percent}}]
+        valid_percent = 100 - properties.pop("s3:invalidPixels_percentage")
+        properties["raster:bands"] = [{"statistics": {"valid_percent": valid_percent}}]
 
         item_dict["stac_extensions"].append(
             "https://stac-extensions.github.io/raster/v1.1.0/schema.json"
@@ -87,10 +98,7 @@ class Collection(pctasks.dataset.collection.Collection):
         # ---- ASSETS ----
         assets = item_dict.pop("assets")
 
-        # FIXME: are we going camelCase -> snake-case for asset keys?
-        # FIXME: Make asset slugs consistent (underscores and lowercase)
-        assets["safe_manifest"] = assets.pop("safe-manifest")
-        assets["safe_manifest"]["title"] = "SAFE product manifest"
+        assets["safe-manifest"]["title"] = "SAFE product manifest"
 
         resolutions = set()
 
@@ -122,8 +130,7 @@ class Collection(pctasks.dataset.collection.Collection):
         assert len(resolutions) == 1, len(resolutions)
         parts = resolutions.pop()
         assert len(parts) == 2, len(parts)
-        resolution = f"{float(parts[0])}x{float(parts[1])}m"
-        properties["s3:spatial_resolution"] = resolution
+        properties["s3:spatial_resolution"] = list(parts)
 
         item = pystac.Item.from_dict(item_dict)
         return [item]
@@ -135,3 +142,10 @@ if __name__ == "__main__":
     storage_factory = StorageFactory()
     [item] = c.create_item(asset_uri, storage_factory)
     item.validate()
+
+    assert item.id == "S3A_OL_2_LFR_20230401T004527_20230401T004655_0088_097_145_1440"
+    assert item.properties["s3:processing_timeliness"] == "NT"
+    assert item.properties["start_datetime"] == "2023-04-01T00:45:26.790564Z"
+    assert item.properties["s3:product_type"] == "OL_2_LFR___"
+    assert item.properties["s3:product_name"] == "LFR"
+    assert item.properties["raster:bands"] == [{"statistics": {"valid_percent": 97.0}}]
