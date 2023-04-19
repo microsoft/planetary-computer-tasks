c71764ec2ed2b99d9584cb4f73646d9c8dd26bea
Tom Augspurger <taugspurger@microsoft.com>
2023-04-19 13:09:33 -0500

    wip - start olci-wfr

---
+++ datasets/sentinel-3/collection/sentinel-3-olci-wfr-l2-netcdf/description.md
@@ -0,0 +1,23 @@
+This collection provides the Full Resolution [OLCI Level-2 Water][olci-l2] products containing data on water-leaving reflectance, ocean color, and more.
+
+## Data files
+
+This dataset includes data on
+
+- Surface directional reflectance
+- Chlorophyll-a concentration
+- Suspended matter concentration
+- Energy flux
+- Aerosol load
+- Integrated water vapor column
+
+Each variable is contained within NetCDF files. Error estimates are available for each product.
+
+## Processing overview
+
+This collection contains Level-2 data in NetCDF files. The values in the data files have been
+converted from Top of Atmosphere radiance to reflectance, and includes various corrections for gaseous absorption and pixel classification.
+See [processing overview](https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-3-olci/level-2/processing) for more.
+
+
+[olci-l2]: https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-3-olci/level-2/land-products
---
+++ datasets/sentinel-3/sentinel_3/sentinel_3_olci_wfr_l2_netcdf.py
@@ -0,0 +1,156 @@
+import decimal
+from pathlib import Path
+from typing import List, Union
+
+import pystac
+
+import pctasks.dataset.collection
+from pctasks.core.models.task import WaitTaskResult
+from pctasks.core.storage import StorageFactory
+
+
+class Collection(pctasks.dataset.collection.Collection):
+    @classmethod
+    def create_item(
+        cls, asset_uri: str, storage_factory: StorageFactory
+    ) -> Union[List[pystac.Item], WaitTaskResult]:
+        # TODO: Check for 0000-000A in sat:platform_international_desigatnor
+        storage, json_path = storage_factory.get_storage_for_file(asset_uri)
+        item_dict = storage.read_json(json_path)
+        asset_directory = Path(item_dict["assets"]["safe-manifest"]["href"]).parent.name
+        assert asset_directory.endswith(".SEN3")
+
+        # Item id contains trailing underscores to pad out a 17 character
+        # instance id
+        item_dict["id"] = item_dict["id"].rstrip("_")
+
+        # ---- PROPERTIES ----
+        properties = item_dict.pop("properties")
+
+        # Add the processing timelessness to the properties
+        timeliness = asset_directory[-11:-9]
+        assert timeliness in ["NR", "ST", "NT"]
+        properties["s3:processing_timeliness"] = timeliness
+
+        # Providers should be supplied in the collection, not the item
+        properties.pop("providers", None)
+
+        # start_datetime and end_datetime are incorrectly formatted
+        properties["start_datetime"] = pystac.utils.datetime_to_str(
+            pystac.utils.str_to_datetime(properties["start_datetime"])
+        )
+        properties["end_datetime"] = pystac.utils.datetime_to_str(
+            pystac.utils.str_to_datetime(properties["end_datetime"])
+        )
+        properties["datetime"] = pystac.utils.datetime_to_str(
+            pystac.utils.str_to_datetime(properties["datetime"])
+        )
+
+        # Do we really want to include s3:mode, which is set to EO (Earth
+        # Observation)? It offers no additional information, IMO.
+        properties.pop("s3:mode", None)
+
+        # FIXME: Do we really want to include s3:gsd for the OLCI and SLSTR
+        # instruments? This is a custom object that is unlikely to be searched.
+        properties.pop("s3:gsd", None)
+
+        # Add a friendly product name for filtering
+        # e.g. 'OL_2_LFR___' -> 'LFR'
+        properties["s3:product_name"] = properties["s3:productType"][5:8]
+
+        # Use underscores instead of camel case for consistency
+        properties["s3:product_type"] = properties.pop("s3:productType")
+        properties["s3:saline_water"] = properties.pop(
+            "s3:salineWaterPixels_percentage"
+        )
+        properties["s3:coastal"] = properties.pop("s3:coastalPixels_percentage")
+        properties["s3:fresh_inland_water"] = properties.pop(
+            "s3:freshInlandWaterPixels_percentage"
+        )
+        properties["s3:tidal_region"] = properties.pop(
+            "s3:tidalRegionPixels_percentage"
+        )
+        properties["s3:land"] = properties.pop("s3:landPixels_percentage")
+        properties["s3:cosmetic"] = properties.pop("s3:cosmeticPixels_percentage")
+        properties["s3:duplicated"] = properties.pop("s3:duplicatedPixels_percentage")
+        properties["s3:saturated"] = properties.pop("s3:saturatedPixels_percentage")
+        properties["s3:dubious"] = properties.pop("s3:dubiousSamples_percentage")
+
+        # use raster for invalid_pixels
+        valid_percent = 100 - properties.pop("s3:invalidPixels_percentage")
+        properties["raster:bands"] = [{"statistics": {"valid_percent": valid_percent}}]
+
+        item_dict["stac_extensions"].append(
+            "https://stac-extensions.github.io/raster/v1.1.0/schema.json"
+        )
+
+        item_dict["properties"] = properties
+
+        # ---- LINKS ----
+        links = item_dict.pop("links")
+
+        # license is the same for all items; include on the collection, not the item
+        for link in links:
+            if link["rel"] == "license":
+                links.remove(link)
+
+        item_dict["links"] = links
+
+        # ---- ASSETS ----
+        assets = item_dict.pop("assets")
+
+        assets["safe-manifest"]["title"] = "SAFE product manifest"
+
+        resolutions = set()
+
+        for v in assets.values():
+            # Remove file:local_path
+            v.pop("file:local_path", None)
+
+            # Move description to title
+            title = v.pop("description", None)
+            if title:
+                v["title"] = title
+
+            resolution = v.pop("resolution", None)
+            if resolution:
+                resolutions.add(tuple(resolution))
+
+            # eo:bands requires the wavelength in micrometers. These are nanometers.
+            eo_bands = v.get("eo:bands", [])
+            for band in eo_bands:
+                for eo_key in ["band_width", "center_wavelength"]:
+                    band[eo_key] = float(decimal.Decimal(band[eo_key]) / 1000)
+
+        item_dict["assets"] = assets
+
+        # FIXME: There is a lonely "resolution" field on the ntc_aod asset that
+        # is not part of the STAC spec. This would normally go in "raster:bands",
+        # but we are not using that extension. Propose moving this to Item
+        # properties under "s3:spatial_resolution", which matches Sentinel-5P.
+        assert len(resolutions) == 1, len(resolutions)
+        parts = resolutions.pop()
+        assert len(parts) == 2, len(parts)
+        properties["s3:spatial_resolution"] = list(parts)
+
+        item = pystac.Item.from_dict(item_dict)
+        return [item]
+
+
+if __name__ == "__main__":
+    c = Collection()
+    asset_uri = "blob://sentinel3euwest/sentinel-3-stac/OLCI/OL_2_WFR___/2023/01/01/S3A_OL_2_WFR_20230101T000030_20230101T000330_0179_094_016_2880.json"  # noqa: E501
+    storage_factory = StorageFactory()
+    [item] = c.create_item(asset_uri, storage_factory)
+    item.validate()
+
+    assert item.id == "S3A_OL_2_WFR_20230101T000030_20230101T000330_0179_094_016_2880"
+    assert item.properties["s3:processing_timeliness"] == "NT"
+    assert item.properties["start_datetime"] == "2023-01-01T00:00:29.538087Z"
+    assert item.properties["s3:product_type"] == "OL_2_WFR___"
+    assert item.properties["s3:product_name"] == "WFR"
+    assert item.properties["raster:bands"] == [{"statistics": {"valid_percent": 96.0}}]
+
+    with open("sentinel-3-olci-wfr-l2.json", "w") as f:
+        import json
+        f.write(json.dumps(item.to_dict(), indent=2))
