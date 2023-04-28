bebde2f972ea3f9e917f59af96bc1d3d6e5a0ddb
pjhartzell <preston.hartzell@gmail.com>
2023-04-28 09:18:19 -0400

    fix: sundry corrections
    - change s3:spatial_resolution from string to list of integers
    - add s3:product_name
    - make asset keys kebab-case
    - remove redundant asset title prefixes
    - round geometry and bbox coordinates to original precision after
      antimeridian fix

--- datasets/sentinel-5p/sentinel_5p.py
+++ datasets/sentinel-5p/sentinel_5p.py
@@ -1,7 +1,7 @@
 import logging
 import re
 from pathlib import Path
-from typing import List, Union
+from typing import Any, List, Union
 
 import antimeridian
 import pystac
@@ -20,35 +20,35 @@ FILENAME_EXPR = re.compile(
 )
 
 ABOUT_LINKS = {
-    "aer_ai": "http://www.tropomi.eu/data-products/uv-aerosol-index",
-    "aer_lh": "http://www.tropomi.eu/data-products/aerosol-layer-height",
-    "ch4": "http://www.tropomi.eu/data-products/methane",
-    "cloud": "http://www.tropomi.eu/data-products/cloud",
-    "co": "http://www.tropomi.eu/data-products/carbon-monoxide",
-    "hcho": "http://www.tropomi.eu/data-products/formaldehyde",
-    "no2": "http://www.tropomi.eu/data-products/nitrogen-dioxide",
-    "o3": "http://www.tropomi.eu/data-products/total-ozone-column",
-    "o3_tcl": "http://www.tropomi.eu/data-products/tropospheric-ozone-column",
-    "so2": "http://www.tropomi.eu/data-products/sulphur-dioxide",
-    "npbd3": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
-    "npbd6": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
-    "npbd7": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
+    "L2__AER_AI": "http://www.tropomi.eu/data-products/uv-aerosol-index",
+    "L2__AER_LH": "http://www.tropomi.eu/data-products/aerosol-layer-height",
+    "L2__CH4___": "http://www.tropomi.eu/data-products/methane",
+    "L2__CLOUD_": "http://www.tropomi.eu/data-products/cloud",
+    "L2__CO____": "http://www.tropomi.eu/data-products/carbon-monoxide",
+    "L2__HCHO__": "http://www.tropomi.eu/data-products/formaldehyde",
+    "L2__NO2___": "http://www.tropomi.eu/data-products/nitrogen-dioxide",
+    "L2__O3____": "http://www.tropomi.eu/data-products/total-ozone-column",
+    "L2__O3_TCL": "http://www.tropomi.eu/data-products/tropospheric-ozone-column",
+    "L2__SO2___": "http://www.tropomi.eu/data-products/sulphur-dioxide",
+    "L2__NP_BD3": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
+    "L2__NP_BD6": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
+    "L2__NP_BD7": "https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-5p/products-algorithms",  # noqa
 }
 
 ASSET_TITLES = {
-    "aer_ai": "TROPOMI/S5P L2 Ultraviolet Aerosol Index",
-    "aer_lh": "TROPOMI/S5P L2 Aerosol Layer Height",
-    "ch4": "TROPOMI/S5P L2 Methane Total Column",
-    "cloud": "TROPOMI/S5P L2 Cloud Fraction, Albedo, and Top Pressure",
-    "co": "TROPOMI/S5P L2 Carbon Monoxide Total Column",
-    "hcho": "TROPOMI/S5P L2 Formaldehyde Total Column",
-    "no2": "TROPOMI/S5P L2 Nitrogen Dioxide Total Column",
-    "o3": "TROPOMI/S5P L2 Ozone Total Column",
-    "o3_tcl": "TROPOMI/S5P L2 Ozone Tropospheric Column",
-    "so2": "TROPOMI/S5P L2 Sulphur Dioxide Total Column",
-    "npbd3": "TROPOMI/S5P VIIRS/NPP Band 3 Cloud Mask",
-    "npbd6": "TROPOMI/S5P VIIRS/NPP Band 6 Cloud Mask",
-    "npbd7": "TROPOMI/S5P VIIRS/NPP Band 7 Cloud Mask",
+    "L2__AER_AI": "Ultraviolet Aerosol Index",
+    "L2__AER_LH": "Aerosol Layer Height",
+    "L2__CH4___": "Methane Total Column",
+    "L2__CLOUD_": "Cloud Fraction, Albedo, and Top Pressure",
+    "L2__CO____": "Carbon Monoxide Total Column",
+    "L2__HCHO__": "Formaldehyde Total Column",
+    "L2__NO2___": "Nitrogen Dioxide Total Column",
+    "L2__O3____": "Ozone Total Column",
+    "L2__O3_TCL": "Ozone Tropospheric Column",
+    "L2__SO2___": "Sulphur Dioxide Total Column",
+    "L2__NP_BD3": "VIIRS/NPP Band 3 Cloud Mask",
+    "L2__NP_BD6": "VIIRS/NPP Band 6 Cloud Mask",
+    "L2__NP_BD7": "VIIRS/NPP Band 7 Cloud Mask",
 }
 
 O3_TCL_GEOMETRY = shapely.geometry.mapping(
@@ -64,7 +64,30 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-class Sentinel5pNetCDFCollection(Collection):
+def recursive_round(coordinates: List[Any], precision: int) -> List[Any]:
+    """Rounds a list of numbers. The list can contain additional nested lists
+    or tuples of numbers.
+
+    Any tuples encountered will be converted to lists.
+
+    Args:
+        coordinates (List[Any]): A list of numbers, possibly containing nested
+            lists or tuples of numbers.
+        precision (int): Number of decimal places to use for rounding.
+
+    Returns:
+        List[Any]: The list of numbers rounded to the given precision.
+    """
+    rounded: List[Any] = []
+    for value in coordinates:
+        if isinstance(value, (int, float)):
+            rounded.append(round(value, precision))
+        else:
+            rounded.append(recursive_round(list(value), precision))
+    return rounded
+
+
+class Sentinel5pNetCDFCollection(Collection):  # type: ignore
     @classmethod
     def create_item(
         cls, asset_uri: str, storage_factory: StorageFactory
@@ -77,10 +100,14 @@ class Sentinel5pNetCDFCollection(Collection):
         match = FILENAME_EXPR.match(Path(netcdf_filename).stem)
         if not match:
             raise ValueError(f"Could not parse filename {Path(json_path).stem}")
-        product = match.groupdict()["product"].strip("_").lower()
-        if product.startswith("np"):
-            product = product.replace("_", "")
+
+        prefix = match.groupdict()["product"].strip("_").lower()
+        if prefix.startswith("np"):
+            prefix = prefix.replace("_", "")
+
         collection_identifier = match.groupdict()["collection"]
+        product_type = item_dict["properties"]["s5p:product_type"]
+        product_name = product_type.lstrip("L2_").rstrip("_").lower().replace("_", "-")
 
         # ---- PROPERTIES ----
         properties = item_dict.pop("properties")
@@ -90,33 +117,36 @@ class Sentinel5pNetCDFCollection(Collection):
 
         # combine the product custom properties to a single object
         product_custom_fields = {}
-        keys = [k for k in properties.keys() if str(k).startswith(product)]
+        keys = [k for k in properties.keys() if str(k).startswith(prefix)]
         if keys:
             for key in keys:
                 product_custom_fields[
-                    str(key).replace(f"{product}:", "")
+                    str(key).replace(f"{prefix}:", "")
                 ] = properties.pop(key)
-            properties[f"s5p:{product}"] = product_custom_fields
+            properties[f"s5p:{prefix}"] = product_custom_fields
 
         # add sentinel-5p collection_identifier
         properties["s5p:collection_identifier"] = collection_identifier
 
-        # clean up spatial_resolution
+        # convert spatial resolution to meters, store in list to match sentinel-3
+        # order is [height, width], aka [along track, across track]
         resolution = properties["s5p:spatial_resolution"]
         resolution = resolution.strip().strip("km2").strip("km").strip()
         parts = resolution.split("x")
         assert len(parts) == 2
-        resolution = f"{float(parts[0])}x{float(parts[1])}km"
-        properties["s5p:spatial_resolution"] = resolution
+        properties["s5p:spatial_resolution"] = [int(float(p)) * 1000 for p in parts]
 
         # correct bad datetimes
         for k, v in properties.items():
             if k.endswith("datetime") and v.endswith("ZZ"):
                 properties[k] = v[:-2] + "Z"
-        if f"s5p:{product}" in properties:
-            for k, v in properties[f"s5p:{product}"].items():
+        if f"s5p:{prefix}" in properties:
+            for k, v in properties[f"s5p:{prefix}"].items():
                 if k.endswith("datetime") and v.endswith("ZZ"):
-                    properties[f"s5p:{product}"][k] = v[:-2] + "Z"
+                    properties[f"s5p:{prefix}"][k] = v[:-2] + "Z"
+
+        # add a user-friendly product name
+        properties["s5p:product_name"] = product_name
 
         item_dict["properties"] = properties
 
@@ -126,9 +156,9 @@ class Sentinel5pNetCDFCollection(Collection):
         # the supplied asset description is too brief to be a description and
         # too inconsistent to use as a title; use a custom title instead
         asset.pop("description")
-        asset["title"] = ASSET_TITLES[product]
+        asset["title"] = ASSET_TITLES[product_type]
 
-        item_dict["assets"][product] = asset
+        item_dict["assets"][product_name] = asset
 
         # ---- LINKS ----
         links = item_dict.pop("links")
@@ -139,20 +169,20 @@ class Sentinel5pNetCDFCollection(Collection):
                 links.remove(link)
 
         # add a unique link to the product landing page
-        about_href = ABOUT_LINKS.get(product)
         links.append(
             {
                 "rel": "about",
-                "href": about_href,
+                "href": ABOUT_LINKS.get(product_type),
                 "type": "text/html",
             }
         )
 
         item_dict["links"] = links
 
+        # ---- GEOMETRY ----
         # fix antimeridian, except for o3_tcl, where we do some hardcode hacks instead
         item = pystac.Item.from_dict(item_dict)
-        if product == "o3_tcl":
+        if product_name == "o3-tcl":
             item.geometry = O3_TCL_GEOMETRY
             item.bbox = O3_TCL_BBOX
         else:
@@ -160,5 +190,9 @@ class Sentinel5pNetCDFCollection(Collection):
             geometry = antimeridian.fix_polygon(polygon)
             item.bbox = list(geometry.bounds)
             item.geometry = shapely.geometry.mapping(geometry)
+            item.bbox = recursive_round(item.bbox, precision=6)
+            item.geometry["coordinates"] = recursive_round(  # type: ignore
+                list(item.geometry["coordinates"]), precision=6  # type: ignore
+            )
 
         return [item]
