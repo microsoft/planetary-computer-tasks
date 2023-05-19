0504cd45025eaaf343a8d8ea9368d869da561c5c
Tom Augspurger <taugspurger@microsoft.com>
2023-05-19 08:06:33 -0500

    Revert "Revert "Set PCTASKS_COSMOSDB__TEST_CONTAINER_SUFFIX in Functions""
    This reverts commit e7448e5f1044d4ff4925222cbde226b35bcca610.

--- pctasks/core/pctasks/core/cosmos/settings.py
+++ pctasks/core/pctasks/core/cosmos/settings.py
@@ -46,35 +46,23 @@ class CosmosDBSettings(PCTasksSettings):
     max_bulk_put_size: int = 250
 
     def get_workflows_container_name(self) -> str:
-        if self.test_container_suffix:
-            return f"tmp-{self.workflows_container_name}-{self.test_container_suffix}"
-        return self.workflows_container_name
+        return f"{self.workflows_container_name}{self.test_container_suffix}"
 
     def get_workflow_runs_container_name(self) -> str:
-        if self.test_container_suffix:
-            return (
-                f"tmp-{self.workflow_runs_container_name}"
-                f"-{self.test_container_suffix}"
-            )
-        return self.workflow_runs_container_name
+        return f"{self.workflow_runs_container_name}{self.test_container_suffix}"
 
     def get_storage_events_container_name(self) -> str:
-        if self.test_container_suffix:
-            return (
-                f"tmp-{self.storage_events_container_name}"
-                f"-{self.test_container_suffix}"
-            )
-        return self.storage_events_container_name
+        return f"{self.storage_events_container_name}{self.test_container_suffix}"
 
     def get_items_container_name(self) -> str:
-        if self.test_container_suffix:
-            return f"tmp-{self.items_container_name}" f"-{self.test_container_suffix}"
-        return self.items_container_name
+        return f"{self.items_container_name}{self.test_container_suffix}"
 
     def get_records_container_name(self) -> str:
-        if self.test_container_suffix:
-            return f"tmp-{self.records_container_name}-{self.test_container_suffix}"
-        return self.records_container_name
+        return f"{self.records_container_name}{self.test_container_suffix}"
+
+    @validator("test_container_suffix", always=True)
+    def _validate_test_container_suffix(cls, v: Optional[str]) -> str:
+        return v or ""
 
     @validator("connection_string")
     def _validate_connection_string(cls, v: Optional[str]) -> Optional[str]:
