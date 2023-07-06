a92017e353bfd1533ae0936ded65580c6e472ee6
Tom Augspurger <taugspurger@microsoft.com>
2023-07-06 11:46:04 -0500

    lint

--- pctasks/core/tests/utils/test_backoff.py
+++ pctasks/core/tests/utils/test_backoff.py
@@ -1,21 +1,23 @@
+import azure.core.exceptions
 import pytest
 import requests.exceptions
-import azure.core.exceptions
 
 from pctasks.core.utils.backoff import with_backoff
 
 
-@pytest.mark.parametrize('kind', [
-    TimeoutError,
-    requests.exceptions.ConnectionError,
-    azure.core.exceptions.IncompleteReadError,
-])
+@pytest.mark.parametrize(
+    "kind",
+    [
+        TimeoutError,
+        requests.exceptions.ConnectionError,
+        azure.core.exceptions.IncompleteReadError,
+    ],
+)
 def test_retry_timeout_errors(kind):
 
     i = 0
 
     def make_callable(kind):
-
         def fn():
             nonlocal i
             i += 1
@@ -24,10 +26,9 @@ def test_retry_timeout_errors(kind):
                 return True
             else:
                 raise kind()
+
         return fn
-        
 
     result = with_backoff(make_callable(kind))
     assert i == 3
     assert result is True
-
