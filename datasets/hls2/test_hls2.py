f1b94af0594d700d18796b5f6773b7cadd9e91f4
Marc Lichtman <marcll@vt.edu>
2025-04-09 13:38:12 -0400

    Add hls2 dataset (#320)
    * first cut
    
    * storage account name
    
    * switch to using two collections
    
    * rename to hls2
    
    * fix depth
    
    * working!
    
    * switched blob:// to href://
    
    * filled in item_assets for both
    
    * add msft:group_id
    
    * readme
    
    * readme
    
    * verify all assets exist in blob
    
    * rename
    
    * default render configs for hls2
    
    * Tanya's render configs
    
    * Document publishing HLS2 collection configs
    
    * readme
    
    * readme
    
    * disable integration tests for now
    
    * don't use pydantic json serialization
    
    * format template and add thumbnails
    
    * fix CLI issues
    
    * all sentinel-2 render_options added to hls2-s30
    
    * remove no_data=0
    
    * convert hls2-l30  render_options to match hls2-s30
    
    * Add sentinel-2c and make all platforms arrays
    
    * test as unit tests with pytest
    
    * set collection ground sampling distance
    
    * description update
    
    * Code change to support python 3.12
    
    * format code
    
    * GOES-19 CMI
    
    * mosaics
    
    * Add ESA as a producer
    
    * Undo goes changes
    
    * delete goes19 only workflow
    
    * clean up readme and dockerfile
    
    ---------
    
    Co-authored-by: Marc Lichtman <marclichtman@microsoft.com>
    Co-authored-by: Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
    Co-authored-by: Tanya Marton <tanyamarton@microsoft.com>
    Co-authored-by: Gustavo Hidalgo <guhidalgo@microsoft.com>

---
+++ datasets/hls2/test_hls2.py
@@ -0,0 +1,44 @@
+from pystac import Item
+import hls2
+import pytest
+from pctasks.core.storage import StorageFactory
+
+test_storage_account = "hls2euwest"
+test_container = "hls2"
+
+@pytest.mark.parametrize(
+    "asset_uri",
+    [
+        f"blob://{test_storage_account}/{test_container}/L30/05/U/LB/2025/02/26/HLS.L30.T05ULB.2025057T212139.v2.0/HLS.L30.T05ULB.2025057T212139.v2.0.jpg"
+    ],
+)
+def test_hls2_landsat(asset_uri: str) -> None:
+    result = hls2.HLS2Collection.create_item(asset_uri, StorageFactory(), upload=False)
+    assert result
+    item, = result
+    assert isinstance(item, Item)
+    assert item.assets["B01"].href == f'https://{test_storage_account}.blob.core.windows.net/{test_container}/L30/05/U/LB/2025/02/26/HLS.L30.T05ULB.2025057T212139.v2.0/HLS.L30.T05ULB.2025057T212139.v2.0.B01.tif'  # noqa: E501
+    assert len(item.stac_extensions) == 4
+    assert item.properties["platform"] == "landsat-9"
+    assert len(item.assets) == 16
+    assert "thumbnail" in item.assets
+    item.validate()
+
+@pytest.mark.parametrize(
+    "asset_uri",
+    [
+        f"blob://{test_storage_account}/{test_container}/S30/05/M/KR/2025/01/05/HLS.S30.T05MKR.2025005T204621.v2.0/HLS.S30.T05MKR.2025005T204621.v2.0.jpg"
+    ],
+)
+def test_hls2_sentinel(asset_uri: str) -> None:
+    result = hls2.HLS2Collection.create_item(asset_uri, StorageFactory(), upload=False)
+    assert result
+    item, = result
+    assert isinstance(item, Item)
+    assert item
+    assert item.assets["B01"].href == f'https://{test_storage_account}.blob.core.windows.net/{test_container}/S30/05/M/KR/2025/01/05/HLS.S30.T05MKR.2025005T204621.v2.0/HLS.S30.T05MKR.2025005T204621.v2.0.B01.tif'  # noqa: E501
+    assert len(item.stac_extensions) == 4
+    assert item.properties["platform"] == "sentinel-2a"
+    assert len(item.assets) == 19 # 18 bands and 1 thumbnail
+    assert "thumbnail" in item.assets
+    item.validate()
