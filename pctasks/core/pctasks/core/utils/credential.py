b8a6759b1f95fdc187ce2fa4626db4302f493870
Manish Kumar Gupta <manishkg@microsoft.com>
2025-08-07 11:34:57 -0700

    Refactor credential handling to use DefaultAzureCredential directly

--- pctasks/core/pctasks/core/utils/credential.py
+++ pctasks/core/pctasks/core/utils/credential.py
@@ -1,17 +1,6 @@
 
-from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
+from azure.identity import DefaultAzureCredential
 import os
 
-def get_credential(
-    use_managed_identity: bool | None = None, use_azure_cli: bool | None = None
-) ->  DefaultAzureCredential:
-    """
-    Get the appropriate Azure credential based on environment variables or defaults.
-    """
-    if use_managed_identity is None:
-        use_managed_identity = os.environ.get("AZURE_CLIENT_ID") is not None
-
-    if use_managed_identity:
-        return ManagedIdentityCredential()
-    else:
-        return DefaultAzureCredential() # CodeQL [SM05139] In Production Managed Identity is set and this is not used in production, so this is safe.
+def get_credential() ->  DefaultAzureCredential:
+    return DefaultAzureCredential() # CodeQL [SM05139] In Production environments, managed identity is already set up, so we can use DefaultAzureCredential directly.
