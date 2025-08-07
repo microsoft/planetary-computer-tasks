e5ff4e10a69ab57fbfbf329a78739b57a179738a
Manish Kumar Gupta <manishkg@microsoft.com>
2025-08-07 08:11:02 -0700

    Refactor get_credential function to directly import Azure credential classes

--- pctasks/core/pctasks/core/utils/credential.py
+++ pctasks/core/pctasks/core/utils/credential.py
@@ -1,10 +1,13 @@
 
-import azure.identity
+from azure.identity import ManagedIdentityCredential, AzureCliCredential, DefaultAzureCredential
 import os
 
 def get_credential(
     use_managed_identity: bool | None = None, use_azure_cli: bool | None = None
-) -> azure.core.credentials.TokenCredential:
+) ->  ManagedIdentityCredential | AzureCliCredential | DefaultAzureCredential:
+    """
+    Get the appropriate Azure credential based on environment variables or defaults.
+    """
     if use_managed_identity is None:
         use_managed_identity = os.environ.get("PCONBOARDING_USE_MANAGED_IDENTITY")
 
@@ -12,8 +15,8 @@ def get_credential(
         use_azure_cli = os.environ.get("PCONBOARDING_USE_AZURE_CLI")
 
     if use_managed_identity:
-        return azure.identity.ManagedIdentityCredential()
+        return ManagedIdentityCredential()
     elif use_azure_cli:
-        return azure.identity.AzureCliCredential()
+        return AzureCliCredential()
     else:
-        return azure.identity.DefaultAzureCredential() # CodeQL [SM05139] In Production Managed Identity is set and this is not used in production, so this is safe.
+        return DefaultAzureCredential() # CodeQL [SM05139] In Production Managed Identity is set and this is not used in production, so this is safe.
