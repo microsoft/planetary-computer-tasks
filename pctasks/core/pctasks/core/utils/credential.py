632a80716a3e0c3a8815fcb269accfe63c0b92ef
Manish Kumar Gupta <manishkg@microsoft.com>
2025-08-07 08:27:07 -0700

    Fix credential helper

--- pctasks/core/pctasks/core/utils/credential.py
+++ pctasks/core/pctasks/core/utils/credential.py
@@ -1,22 +1,17 @@
 
-from azure.identity import ManagedIdentityCredential, AzureCliCredential, DefaultAzureCredential
+from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
 import os
 
 def get_credential(
     use_managed_identity: bool | None = None, use_azure_cli: bool | None = None
-) ->  ManagedIdentityCredential | AzureCliCredential | DefaultAzureCredential:
+) ->  DefaultAzureCredential:
     """
     Get the appropriate Azure credential based on environment variables or defaults.
     """
     if use_managed_identity is None:
-        use_managed_identity = os.environ.get("PCONBOARDING_USE_MANAGED_IDENTITY")
-
-    if use_azure_cli is None:
-        use_azure_cli = os.environ.get("PCONBOARDING_USE_AZURE_CLI")
+        use_managed_identity = os.environ.get("AZURE_CLIENT_ID") is not None
 
     if use_managed_identity:
         return ManagedIdentityCredential()
-    elif use_azure_cli:
-        return AzureCliCredential()
     else:
         return DefaultAzureCredential() # CodeQL [SM05139] In Production Managed Identity is set and this is not used in production, so this is safe.
