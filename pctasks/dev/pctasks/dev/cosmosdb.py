01dcb94e086ecf38216a5d2e4557c2e52697edcf
Karthick <karthick.narendran@microsoft.com>
2025-10-01 15:48:06 +0100

    Fix for S360 alerts

--- pctasks/dev/pctasks/dev/cosmosdb.py
+++ pctasks/dev/pctasks/dev/cosmosdb.py
@@ -34,7 +34,7 @@ def wait_for_emulator_start(url: str) -> None:
         try_count = 0
         while True:
             try:
-                resp = requests.get(url, verify=False)
+                resp = requests.get(url, verify=False)  # CodeQL [SM03157] justification
                 resp.raise_for_status()
                 break
             except Exception as e:
