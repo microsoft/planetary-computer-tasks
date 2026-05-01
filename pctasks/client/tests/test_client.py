702c15ce763d9489eec416a1d73b9bd6151b0140
Manish Kumar Gupta <manishkg@microsoft.com>
2026-05-01 11:48:07 -0700

    experimenting with Copilot suggestions

--- pctasks/client/tests/test_client.py
+++ pctasks/client/tests/test_client.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 import pytest
@@ -26,7 +27,7 @@ def test_client_submit():
                 tasks=[
                     TaskDefinition(
                         id="submit_unit_test",
-                        image="pctasks-ingest:latest",
+                        image= os.environ.get("PCTASKS_INGEST_IMAGE", "pctasks-ingest:latest"),
                         code=CodeConfig(src=str(code)),
                         task="mycode:MyMockTask",
                         args={"result_path": "/dev/null"},
